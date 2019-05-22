9317315bb0bce3745aaac5401af790d54a67e060
Raul RCXO. Corona Xolaltenco <raul.corona@stkprojects.com>
2019-05-22 15:41:21 -0500

    agregando utils

--- mobile-app-ws/src/main/java/com/appkardumen/app/ws/shared/Utils.java
+++ mobile-app-ws/src/main/java/com/appkardumen/app/ws/shared/Utils.java
@@ -2,6 +2,9 @@ package com.appkardumen.app.ws.shared;
 
 import java.util.UUID;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Utils {
 	public String generateUserId() {
 		return UUID.randomUUID().toString();
